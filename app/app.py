f5631688d4020af12280ce9760dd33d87bcfc8bc
Tom Brooks tombrooks248@gmail.com <tombrooks248@gmail.com>
2024-10-08 17:21:04 +0100

    got basic flask running

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
 from get_current_gen_data import get_current_gen_data
 import pandas
 
+from flask import Flask
+app = Flask(__name__)
+
+
+
 gen_data = get_current_gen_data()
 
+
+@app.route('/')
+def home():
+    return "Hello world!"
 
 
 if __name__ == "__main__":
