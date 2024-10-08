31230e1c8de6f2bb579d27f215d12bb25fdf03f0
Tom Brooks tombrooks248@gmail.com <tombrooks248@gmail.com>
2024-10-08 23:17:30 +0100

    added home.html into templates folder and got it working

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from get_current_gen_data import get_current_gen_data
 import pandas
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -11,8 +11,7 @@ gen_data = get_current_gen_data()
 
 @app.route('/')
 def home():
-    return "Hello world!"
-
+    return render_template('home.html')
 
 if __name__ == "__main__":
     print(gen_data)
